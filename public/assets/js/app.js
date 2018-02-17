6bfd340fa2383aec0b06b97638ab908e9af503e5
shulme801 <shulme801@gmail.com>
2018-02-17 06:33:35 -0500

    17 Feb Basic Functionality

---
+++ public/assets/js/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+  // Nav Bar Mobile Slider
+  $(".button-collapse").sideNav();
+
+  // Here we add a comment when the add comment button is clicked
+  $('.add-comment-button').on('click', function(){
+
+    // Get _id of comment to be deleted
+    var articleId = $(this).data("id");
+
+    // get the baseURL for either local host or  Heroku
+    var baseURL = window.location.origin;
+
+    // Get Form Data by Id
+    var frmName = "form-add-" + articleId;
+    var frm = $('#' + frmName);
+
+
+    // use AJAX call to delete the comment
+    $.ajax({
+      url: baseURL + '/add/comment/' + articleId,
+      type: 'POST',
+      data: frm.serialize(),
+    })
+    .done(function() {
+      // and refresh the window after the call has completed
+      location.reload();
+    });
+    
+    // prevent default behavior
+    return false;
+
+  });
+
+
+  // this is the click listener to delete a comment
+  $('.delete-comment-button').on('click', function(){
+
+    // Get the id of comment to be deleted
+    var commentId = $(this).data("id");
+
+    // get the baseURL for either local host or  Heroku
+    var baseURL = window.location.origin;
+
+    // AJAX call to delete a comment
+    $.ajax({
+      url: baseURL + '/remove/comment/' + commentId,
+      type: 'POST',
+    })
+    .done(function() {
+      // and refresh the window after the call has completed
+      location.reload();
+    });
+    
+    // prevent default behavior
+    return false;
+
+  });
+  
+});
